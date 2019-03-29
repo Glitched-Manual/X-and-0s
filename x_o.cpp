a3ccc8ac49d4994efbe6be03a5dea0d4ff6f7d12
Green Star <greenstar@localhost.localdomain>
2019-03-28 22:16:17 -0400

    added classes position type, Tile class, Grid class and Gameloop class.

--- x_o.cpp
+++ x_o.cpp
@@ -2,12 +2,22 @@
 #include <vector>
 #include <string>
 
-class Tile{
+Typedef class{
+public:
+int x;
+int y;
+}Position;
 
+class Tile{
+Position tile_position;
+bool title_marked;
+std::string tile_mark;
+public:
+Postion GetTilePosition(){return tile_position;}
 };
 
 class Grid{
-
+Title[3][3];
 
 };
 class GameLoop{
