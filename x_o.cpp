c6ec39c3061238f4ffad4a8b2d1c56cdcec17fff
Green Star <greenstar@localhost.localdomain>
2019-04-02 15:04:14 -0400

    display added

--- x_o.cpp
+++ x_o.cpp
@@ -1,90 +1,203 @@
 #include <iostream>
+
 #include <vector>
+
 #include <string>
 
+
 class Position{
+
 public:
+
 int x;
+
 int y;
+
 };
+
 
 class Win_Case{
+
 Position combination[3];
+
 std::string win_case_id;
+
 public:
+
 void SetPositionCombination();
+
 Position* GetCombination(){ return combination; }
+
 std::string GetWinCaseID(){ return win_case_id; }
+
 };
+
 
 void Win_Case::SetPositionCombination()
+
 {
+
 }
 
+
 class Tile{
+
 Position tile_position;
+
 bool tile_marked;
+
 std::string tile_mark;
+
 public:
+
+Tile();
+void SetTileMark(std::string passed_mark){ tile_mark = passed_mark;}
+std::string GetTileMark(){ return tile_mark; }
+
 void SetTilePosition(int passed_x, int passed_y);
+
 Position GetTilePosition(){return tile_position;}
+
 };
-void Tile ::SetTilePosition(int passed_x, int passed_y)
+
+Tile::Tile()
 {
-tile_position.x = passed_x;
-tile_position.y = passed_y;
+tile_mark = "?";
 }
+
+void Tile ::SetTilePosition(int passed_x, int passed_y)
+
+{
+
+tile_position.x = passed_x;
+
+tile_position.y = passed_y;
+
+}
+
 
 
 class Grid{
+
 Tile game_tiles[3][3];
+
 public:
-//what?
-void SetGameTile(int passed_x, int passed_y);
+
+Grid();
+
+void DisplayGrid();
+
+void SetGameTiles();
+
 Tile GetGameTiles(int passed_x, int passed_y){return game_tiles[passed_x][passed_y];}
+
 };
 
-void Grid::SetGameTile(int passed_x, int passed_y)
+Grid ::Grid()
 {
-   if((passed_x < 3)&(passed_y < 3))
-   {
-    game_tiles[passed_x][passed_y].SetTilePosition(passed_x,passed_y);
-   }
+    SetGameTiles();
+}
+
+void Grid::SetGameTiles()
+{ 
+    for(unsigned int yi = 0; yi < 3; yi++)
+    {
+        for(unsigned int xi = 0; xi < 3; xi++)
+        {
+             game_tiles[xi][yi].SetTilePosition(xi,yi);
+        }
+    }
+}
+
+void Grid::DisplayGrid()
+{
+/*
+
+ x | x | x |\n
+ ---------- \n
+ x | x | x |\n
+ ---------- \n
+ x | x | x |\n
+*/
+
+
+for(unsigned int yi = 0; yi < 3; yi++)
+    {
+        for(unsigned int xi = 0; xi < 3; xi++)
+        {
+            // game_tiles[xi][yi].SetTilePosition(xi,yi);
+           std::cout << " " << game_tiles[xi][yi].GetTileMark() << " |";
+          //if x == 2 add "\n" and "-----\n"  
+          if(xi==2)
+	  {
+	   std::cout<< "\n ----------\n";
+	  }
+        }
+    }
+
 }
 
 class Game{
+
 bool gameover;
+
 Grid game_grid;
+
 Win_Case win_cases[8];
+
 public:
+
 Grid GetGameGrid(){ return game_grid;}
+
 void PlayerWin(Position passed_position);
+
 void LoadWinCases();
+
 };
+
 void LoadWinCases()
+
 {
+
 //win_cases[0].
+
 }
 
+
 void Game::PlayerWin(Position passed_position)
+
 {
+
 //get play position -> passed_position
+
 
 //check win cases if position is included
 
+
 //check win case if all three positions are marked by player
 
+
 //if all three marked by player
+
 }
+
 /*
+
 int arr[3][1];
+
 */
 
 int main()
 {
+
 Game game;
- 
-game.GetGameGrid().SetGameTile(0,0);
+//game.GetGameGrid().SetGameTile(0,0);
+
+game.GetGameGrid().DisplayGrid();
 std::cout << "it works" << std::endl;
+
 return 0;
+
 }
+
+
