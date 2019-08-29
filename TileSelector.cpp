08174714094ecc33bf3cd0ff96340b524342d0ad
M R <popcorneater720@gmail.com>
2019-08-29 17:37:18 -0400

    test Controls working with reconstructed game. Play again and Quit match feature needed

---
+++ TileSelector.cpp
@@ -0,0 +1,119 @@
+#include "TileSelector.h"
+
+TileSelector::TileSelector(LoaderParams* pParams, Grid* passed_grid, SDL_Rect* passed_hash_table_rect, CSDL* passed_csdl_obj, std::string passed_id_name)
+{
+	tile_selector_texture_rect = new SDL_Rect;
+
+	//LoaderParams
+
+	tile_selector_texture_rect->x = pParams->GetX() - (pParams->GetWidth() / 2);
+	tile_selector_texture_rect->y = pParams->GetY() - (pParams->GetHeight() / 2);
+	tile_selector_texture_rect->w = pParams->GetWidth();
+	tile_selector_texture_rect->h = pParams->GetHeight();
+
+	//get grid obj
+	     
+	 game_grid = passed_grid;
+
+	//get hash table rect
+
+	 hash_table_rect = passed_hash_table_rect;
+
+	 //get csdl obj by pointer
+
+	 csdl_obj = passed_csdl_obj;
+
+	//set string id name
+
+	 tile_selector_id = passed_id_name;
+
+}
+
+
+
+bool TileSelector::LoadGameObjectContent(SDL_Renderer* passed_Renderer)
+{
+	//open image
+	tile_selector_texture = IMG_LoadTexture(passed_Renderer, "tile_selection_image.png");
+
+	if (tile_selector_texture == NULL)
+	{
+
+		if (debug.is_debug_mode())
+		{
+			std::cout << "TileSelector::LoadGameObjectContent() error - " << IMG_GetError() << std::endl;
+		}
+		return false;
+	}
+
+	// positions of tiles from hash table
+
+	// do math on draw for dynamic postions
+
+	//set colors red ,blue,green or black or white
+
+	tile_selector_content_loaded = true;
+
+	return true;
+
+}
+void TileSelector::Draw(SDL_Renderer* passed_Renderer)
+{
+
+	/*
+	
+	if mouse draw using Uint32 SDL_GetMouseState(int* x, int* y);
+
+	*/
+
+	if (tile_selector_content_loaded)
+	{
+		SDL_RenderCopyEx(passed_Renderer, tile_selector_texture, NULL, tile_selector_texture_rect, 0, NULL, SDL_FLIP_NONE);
+	}
+
+
+	
+}
+void TileSelector::Update()
+{
+	/*
+	
+	Needs to change control types 
+	  - if mouse moves on grid mouse control
+	  - if controller button pressed controller
+	  - if keyboard key keyboard arrows
+	*/
+	// 
+
+	//if tile unavailable change color
+
+}
+void TileSelector::CleanGameObjectContent()
+{
+
+}
+
+
+void TileSelector::ToggleTextureColor(Uint8 passed_r_value, Uint8 passed_g_value, Uint8 passed_b_value)
+{
+
+}
+
+void TileSelector::AlterTextureColor(Uint8 passed_r_value, Uint8 passed_g_value, Uint8 passed_b_value)
+{
+
+}
+void TileSelector::RevertAlteredTextureColor()
+{
+
+}
+void TileSelector::SetOriginalColors()
+{
+
+}
+
+void LoadSelectorColors()
+{
+
+
+}
---
+++ TileSelector.h
@@ -0,0 +1,70 @@
+#include "HashTable.h"
+#include "Grid.h"
+#include "CSDL.h"
+#include "Developer.h"
+
+#include <vector>
+
+#ifndef TILESELECTOR_H
+#define TILESELECTOR_H 
+
+class TileSelector: public GameObject
+{
+	SDL_Texture* tile_selector_texture = NULL;
+
+	SDL_Rect* tile_selector_texture_rect = NULL;
+
+	SDL_Rect* hash_table_rect = NULL;
+
+	Grid* game_grid = NULL;
+
+	CSDL* csdl_obj;
+
+	Color tile_selector_base_color;
+
+	std::vector<SDL_Color*> tile_selector_vector;
+
+	std::string tile_selector_id;
+
+	bool tile_selector_texture_texture_colors_altered = false;
+
+	bool tile_selector_content_loaded = false;
+
+	Developer debug;
+
+
+	//for switching controls
+	enum ControlsType {keyboard_controls,mouse_controls,game_controller_controls};
+
+public:
+	TileSelector(LoaderParams* pParams, Grid* passed_grid, SDL_Rect* passed_hash_table_rect, CSDL* passed_csdl_obj, std::string passed_id_name);
+
+	std::string GetGameObjectID() { return tile_selector_id; }
+	 bool LoadGameObjectContent(SDL_Renderer* passed_Renderer);
+	 void Draw(SDL_Renderer* passed_Renderer);
+	 void Update();
+	 void CleanGameObjectContent();
+
+	 bool GetContentLoadedStatus() { return tile_selector_content_loaded; }
+	 SDL_Texture* GetGameTexture() { return tile_selector_texture;	 }
+
+	//if case switches colors for original to other colors and back when recalled
+	 void ToggleTextureColor(Uint8 passed_r_value, Uint8 passed_g_value, Uint8 passed_b_value);
+
+	 void AlterTextureColor(Uint8 passed_r_value, Uint8 passed_g_value, Uint8 passed_b_value);
+	 void RevertAlteredTextureColor();
+	 void SetOriginalColors(); 
+	 Color GetOringinalColors() {	 return tile_selector_base_color;	 } //leave empty return white
+	 bool GetAreColorsAltered() { return tile_selector_texture_texture_colors_altered; }
+	 SDL_Rect* GetTextureRect() { return tile_selector_texture_rect; }
+
+	 //load colors
+
+	 void LoadSelectorColors();
+
+};
+
+
+#endif //TILESELECTOR_H
+
+
