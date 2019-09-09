2c60afa689e4f9c2ed94dc5f1273724855609a35
M R <popcorneater720@gmail.com>
2019-09-09 01:09:12 -0400

    added HitBox Texture image and address

--- HitBox.cpp
+++ HitBox.cpp
@@ -16,14 +16,27 @@ HitBox::~HitBox()
 
 bool HitBox::LoadTexture()
 {
+	//load a colored background from a texture or a image with a x
 
+	hit_box_texture = IMG_LoadTexture(game_renderer, "resources/images/hit_box_image001");
+
+	if (hit_box_texture == NULL)
+	{
+		if (debug.is_debug_mode())
+		{
+			std::cout << "HitBox::LoadTexture() error - " << IMG_GetError() << std::endl;
+		}
+		return false;
+	}
+	return true;
 }
 
 void HitBox::SetRenderer(SDL_Renderer* passed_renderer)
 {
 	game_renderer = passed_renderer;
 }
+
 void HitBox::RenderGameObjectHitBox(SDL_Rect* passed_collision_rect)
 {
-	SDL_RenderCopyEx(game_renderer, hit_box_texture, texture_crop_rect, passed_collision_rect, 0, NULL, SDL_FLIP_NONE);
+	SDL_RenderCopyEx(game_renderer, hit_box_texture, NULL, passed_collision_rect, 0, NULL, SDL_FLIP_NONE);
 }
